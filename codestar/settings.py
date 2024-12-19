9ddcfb211185e46c5082a876f52b52157b42fc07
Tom McKechnie <tommckechnie8@gmail.com>
2024-12-19 15:07:56 +0000

    update ALLOWED_HOSTS for production; add gunicorn to requirements

--- codestar/settings.py
+++ codestar/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-1#ic9!%t0#3f6t0a%31$!4+wo)$_6kbzn1tiys^e1c@)$*o_)5'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '.herokuapp.com']
 
 
 # Application definition
