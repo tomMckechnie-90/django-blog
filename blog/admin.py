2ab13ba6ca0de561be0ece5b50dbdd2ebc70add1
Tom McKechnie <tommckechnie8@gmail.com>
2025-01-02 10:41:21 +0000

    Add CommentAdmin for managing comments and update models for ordering and string representation

--- blog/admin.py
+++ blog/admin.py
@@ -2,6 +2,16 @@ from django.contrib import admin
 from .models import Post
 from .models import Comment
 
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ("author", "post", "body", "approved", "created_on")
+    # list_filter = ("approved", "created_on", "body")
+    # search_fields = ("author__username", "body")
+    # actions = ["approve_comments"]
+
+    def approve_comments(self, request, queryset):
+        queryset.update(approved=True)
+
+
 # Register your models here.
 admin.site.register(Post)
-admin.site.register(Comment)
+admin.site.register(Comment, CommentAdmin)
