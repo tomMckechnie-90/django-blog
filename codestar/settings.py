92348acccb03947957c983f8dfdb99109f1737af
Tom McKechnie <tommckechnie8@gmail.com>
2025-01-07 14:43:01 +0000

    enable serving of image files

--- codestar/settings.py
+++ codestar/settings.py
@@ -28,7 +28,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = 'b%@dzq%dd344r^)24vkc7#n(dew*6r(wcwg^8p1+yz%6ckp)q)'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1', '.herokuapp.com', '8000-tommckechnie-djangoblog-qnj3ipy2cyl.ws.codeinstitute-ide.net']
 
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'cloudinary_storage',
     'django.contrib.sites',
     'allauth',
     'allauth.account',
@@ -49,6 +50,7 @@ INSTALLED_APPS = [
     'crispy_forms',
     'crispy_bootstrap5',
     'django_summernote',
+    'cloudinary',
     'blog',
     'about',
 
